b75a88ef455669870f9f1f6f2f3e9a9ae7fdcdfe
Bárbara Gaito <127039725+BarbaraGaito@users.noreply.github.com>
2024-08-30 23:11:34 -0300

    Commit Inicial

--- src/main/java/com/uade/tpo/demo/repository/CategoryRepository.java
+++ src/main/java/com/uade/tpo/demo/repository/CategoryRepository.java
@@ -1,34 +1,16 @@
 package com.uade.tpo.demo.repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.List;
 
-import com.uade.tpo.demo.Entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public class CategoryRepository {
-    private ArrayList<Category> categories;
+import com.uade.tpo.demo.entity.Category;
 
-    public CategoryRepository() {
-        categories = new ArrayList<Category>(
-                Arrays.asList(Category.builder().description("Electronica").id(1).build(),
-                        Category.builder().description("Cocina").id(2).build(),
-                        Category.builder().description("Gaming").id(3).build()));
-    }
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    public ArrayList<Category> getCategories() {
-        return this.categories;
-    }
-
-    public Optional<Category> getCategoryById(int categoryId) {
-        return this.categories.stream().filter(m -> m.getId() == categoryId).findAny();
-    }
-
-    public Category createCategory(int newCategoryId, String description) {
-        Category newCategory = Category.builder()
-                .description(description)
-                .id(newCategoryId).build();
-        this.categories.add(newCategory);
-        return newCategory;
-    }
+    @Query(value = "select c from Category c where c.description = ?1")
+    List<Category> findByDescription(String description);
 }
