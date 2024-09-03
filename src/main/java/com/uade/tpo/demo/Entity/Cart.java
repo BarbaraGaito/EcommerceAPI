e3dbd68a1a80022f320f08432194d40bf9bcbf90
lara_alonso <laralonso@uade.edu.ar>
2024-09-03 17:14:48 -0300

    Cambios catalogo

--- src/main/java/com/uade/tpo/demo/Entity/Cart.java
+++ src/main/java/com/uade/tpo/demo/Entity/Cart.java
@@ -1,12 +1,22 @@
 package com.uade.tpo.demo.Entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
 
 @Data
 @Entity
+@Builder
 @Table(name = "carts")
 public class Cart {
     @Id
