1f29dbd8ee727f73e4a4848ade902931e653ea8d
Homero Gonzalez <134329872+homerig@users.noreply.github.com>
2024-09-03 02:42:32 -0300

    Tokens y Sesiones v1

--- src/main/java/com/uade/tpo/demo/repository/UserRepository.java
+++ src/main/java/com/uade/tpo/demo/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.uade.tpo.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 import com.uade.tpo.demo.Entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByEmail(String mail);
 }
