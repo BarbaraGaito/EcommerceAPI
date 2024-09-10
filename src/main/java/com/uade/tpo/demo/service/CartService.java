7875b3dd8d296b160265e93eb3165559f904c84e
Fran-Cos <francosentino9@gmai.com>
2024-09-10 01:04:24 -0300

    cambios carrito

--- src/main/java/com/uade/tpo/demo/service/CartService.java
+++ src/main/java/com/uade/tpo/demo/service/CartService.java
@@ -3,15 +3,18 @@ package com.uade.tpo.demo.service;
 import java.util.List;
 
 import com.uade.tpo.demo.Entity.Cart;
+import com.uade.tpo.demo.Entity.dto.CartDTO;
 
 public interface CartService {
     Cart createCart(Cart cart);
-    Cart updateCart(Long id, Cart cart);
+    void updateCart(Long id, Cart cart);
     void deleteCart(Long id);
+    CartDTO getCartByIdDTO(Long id);
     Cart getCartById(Long id);
-    List<Cart> getAllCarts();
-    Cart addProductToCart(Long cartId, Long productId, int quantity);
-    Cart removeProductFromCart(Long cartId, Long productId);
-    Cart updateProductQuantityInCart(Long cartId, Long productId, int quantity);
+    List<CartDTO> getAllCarts();
+    void addProductToCart(Long cartId, Long productId, int quantity);
+    void removeProductFromCart(Long cartId, Long productId);
+    void updateProductQuantityInCart(Long cartId, Long productId, int quantity);
     Double finishCart(Long cartId);
+    
 }
