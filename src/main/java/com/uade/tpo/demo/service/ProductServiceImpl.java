120f3704360e00d88640febcb663b0c71629ff00
Bárbara Gaito <127039725+BarbaraGaito@users.noreply.github.com>
2024-10-20 21:51:08 -0300

    Cambios

--- src/main/java/com/uade/tpo/demo/service/ProductServiceImpl.java
+++ src/main/java/com/uade/tpo/demo/service/ProductServiceImpl.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
